d571478cd7d45d0dff7d97dfb8fdaab2062e36de
tiaL-ops <104815994+tiaL-ops@users.noreply.github.com>
2025-01-13 03:53:16 +0300

    Can hide profile and login

---
+++ public/profile.js
@@ -0,0 +1,3 @@
+import { createAuthForm } from './game/scripts/authform.js';
+import { auth, signOut } from './game/scripts/firebase.js';
+import { getFirestore, doc, getDocs, setDoc, collection, getDoc } from '../public/game/scripts/firebase.js';
