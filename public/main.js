f4e28ff36a9921675f943714ae8a4ca6bd02829b
Landy Rakotoarison <landytiaa@gmail.com>
2025-02-21 19:51:54 -0500

    cmmit before modify the answe

--- public/main.js
+++ public/main.js
@@ -1,9 +1,17 @@
 // main.js
-
+//deploy
+/*
 import { createAuthForm } from './game/scripts/authform.js';
 import { auth, signOut } from '/game/scripts/firebase.js';
 import { getFirestore, doc, getDocs, setDoc, collection, getDoc } from '/game/scripts/firebase.js';
 
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-auth.js";
+*/
+
+//locally
+import { createAuthForm } from './game/scripts/authform.js';
+import { auth, signOut } from './game/scripts/firebase.js';
+import { getFirestore, doc, getDocs, setDoc, collection, getDoc } from '../public/game/scripts/firebase.js';
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-auth.js";
 
 // Initialize Firestore
